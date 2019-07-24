db24ece14c2bdb20b6395dd9bd96bae5e2187a12
Ersel Aker <erselaker@gmail.com>
2019-07-24 20:26:41 +0100

    onchange

--- scripts.js
+++ scripts.js
@@ -27,4 +27,41 @@ window.onload = function() {
       "You will be paying &pound;" + monthlyRepayment + " per month";
     document.querySelector("#mortgage-offer").innerHTML = offer;
   });
+
+  const amountField = document.querySelector("#amount");
+  amountField.addEventListener('change', event => {
+      const amountToBorrow = event.target.value;
+      const paymentPeriodField = document.querySelector("#paymentPeriodInYears");
+      const paymentPeriodInYears = paymentPeriodField.value;
+      
+      const monthlyRepayment = calculateMonthlyRepayment(
+        7,
+        paymentPeriodInYears,
+        amountToBorrow
+      );
+  
+      const offer =
+        "You will be paying &pound;" + monthlyRepayment + " per month";
+      document.querySelector("#mortgage-offer").innerHTML = offer;
+  })
+
+
+
+
+  const paymentPeriodField = document.querySelector("#paymentPeriodInYears");
+  paymentPeriodField.addEventListener('change', event => {
+    // 1 - get the updated value for payment period
+    const paymentPeriod = event.target.value;
+    // 2 - get the value for amount to borrow
+    const amountToBorrow = document.querySelector("#amount").value;
+    // 3 - calculate the amount to repay per month
+    const monthlyRepayment = calculateMonthlyRepayment(
+        7,
+        paymentPeriod,
+        amountToBorrow
+      ); 
+    // 4 - show the amount to the user
+    const offer = "You will be paying &pound;" + monthlyRepayment + " per month";
+    document.querySelector("#mortgage-offer").innerHTML = offer; 
+})
 };
